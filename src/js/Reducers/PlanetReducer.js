114f66af80ecd09ce14493d1af0b1e04cc14a219
Debanjan Dey <db4friends@gmail.com>
2020-04-07 23:23:17 +0530

    added radio buttons

--- src/js/Reducers/PlanetReducer.js
+++ src/js/Reducers/PlanetReducer.js
@@ -1,18 +1,32 @@
+const UNDEF = undefined;
 const initialState = {
     planets : [],
+    remainingPlanets: [],
     selectedPlanets: []
 }
 const fetchPlanets = (state = initialState, action) => {
     switch(action.type) {
         case "SET_PLANETS": return {
             ...state,
-            planets : action.payload
+            planets : action.payload,
+            remainingPlanets :action.payload
         }
-        case "SELECTED_PLANETS" :{ 
-            state.selectedPlanets.push(action.payload);
+        case "SELECTED_PLANETS" :{
+            let foundIndex = UNDEF; 
+            foundIndex = state.selectedPlanets.findIndex((planet) => {
+                return (planet.selectedBy === action.payload.selectedBy)
+            });
+            if(foundIndex !== -1) {
+                state.selectedPlanets.splice(foundIndex,1,action.payload);
+            } else {
+                state.selectedPlanets.push(action.payload);
+            }
             return {
             ...state,
-            selectedPlanets : state.selectedPlanets
+            selectedPlanets : state.selectedPlanets,
+            remainingPlanets: state.planets.filter((planet) => {
+                return !state.selectedPlanets.includes(planet)
+            })
         }
     }
         case "REMAINING_PLANETS" : return {
--- src/js/components/PlanetDropDown.js
+++ src/js/components/PlanetDropDown.js
@@ -11,15 +11,19 @@ class PlanetDropDown extends React.Component {
         let selected = this.props.planets.find((planet)=> {
             return(planet.name === slectedOption.target.value)
         })
+        selected.selectedBy = this.props.destination;
         this.props.planetSelected(selected);
     }
     render() {
+        const selectedPlanet = this.props.selectedPlanets.find((planet) => {
+            return(planet.selectedBy === this.props.destination)
+        });
         return (
             // <div className = "planetsContainer">
             <div className = "planetsDiv">
             <label htmlFor = "planet-select-1">Destination {this.props.destination}</label>
             <select  onChange = {this.handleChange}>
-                {/* <option key = {this.props.data.planets.selected} value = {this.props.data.planets.selected}>{this.props.data.planets.selected}</option> */}
+                <option key = {selectedPlanet ? selectedPlanet.name : "Select"} value = {selectedPlanet ? selectedPlanet.name : "Select"}>{selectedPlanet ? selectedPlanet.name : "Select"}</option>
                 {
                    this.props.planets.map((planet) => {
                         return(
@@ -28,13 +32,32 @@ class PlanetDropDown extends React.Component {
                     })
                 }
             </select>
+            <div className = "radioDiv">
+             <div>   
+            <input type="radio" id="" name="" value=""/> 
+            <label >Hi</label> <br/>
+            </div>
+            <div>
+            <input type="radio" id="" name="" value=""/>
+            <label >Hello</label> <br/>
+            </div>
+            <div>
+            <input type="radio" id="" name="" value=""/>
+            <label >Chakri</label> <br/>
+            </div>
+            <div>
+            <input type="radio" id="" name="" value=""/>
+            <label >Chai</label>
+            </div>
+            </div>
             </div>
         )
     }
 }
 const mapStateToProps = (state) => {
     return {
-        planets: state.fetchPlanets.planets
+        planets: state.fetchPlanets.remainingPlanets,
+        selectedPlanets: state.fetchPlanets.selectedPlanets
     }
 }
 const mapDispatchToProps = (dispatch) => {
